47fda77309ba86e8e4385242c2bd708f09e3dbae
manishkr04 <129091923+manishkr04@users.noreply.github.com>
2024-10-28 01:32:50 +0530

    listing folder

---
+++ list_files_in_folders.py
@@ -0,0 +1,27 @@
+import os
+
+
+def list_files_in_folder(folder_path):
+    try:
+        file = os.listdir(folder_path)
+        return files, None
+    except FileNotFoundError:
+        return None, "Folder not found"
+    except PermissionError:
+        return None, "Permission denied"
+    
+def main():
+    folder_paths = input("Enter a list of folder paths seperated by space: ").split()
+
+    for folder_path in folder_paths:
+        files, error_message = list_files_in_folder(folder_path)
+        if files:
+            print(f"Files in {folder_path}:")
+            for file in files:
+                print(file)
+        else:
+            print(f"Erron in {folder_path}: {error_message}")
+
+if __name__ == "__main__":
+    main()
+    
